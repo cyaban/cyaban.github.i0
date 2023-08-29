81ad2ace9adca8c27cec2ce6b6bd1978a9324335
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-08-29 13:05:50 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,54 +1,47 @@
-// Function to set the page title and store it in local storage
-        function setTitle(title) {
-            document.title = title;
-            localStorage.setItem('pageTitle', title);
-        }
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        // Get references to DOM elements
+        var panicKeyInput = document.getElementById('panicKeyInput');
+        var websiteInput = document.getElementById('websiteInput');
 
-        // Function to reset the page title and clear it from local storage
-        function resetTab() {
-            document.title = ''; // Set an empty title
-            localStorage.removeItem('pageTitle'); // Remove the stored title
-            document.getElementById('title').value = ''; // Clear the input field
-        }
+        // Load stored values
+        loadStoredValues();
 
-        // Initialize the page title from local storage if available
-        document.addEventListener('DOMContentLoaded', function () {
-            var storedTitle = localStorage.getItem('pageTitle');
-            if (storedTitle) {
-                setTitle(storedTitle);
-                document.getElementById('title').value = storedTitle;
-            }
+        // Add event listener for keydown event
+        document.addEventListener('keydown', function (event) {
+            handleKeyPress(event);
         });
-      <script>
-            document.addEventListener('DOMContentLoaded', function () {
-                var storedPanicKey = localStorage.getItem('panicKey');
-                var storedWebsiteURL = localStorage.getItem('websiteURL');
 
-                var panicKeyInput = document.getElementById('panicKeyInput');
-                var websiteInput = document.getElementById('websiteInput');
+        // Function to load stored values from localStorage
+        function loadStoredValues() {
+            var storedPanicKey = localStorage.getItem('panicKey');
+            var storedWebsiteURL = localStorage.getItem('websiteURL');
 
-                // Set the input fields to the stored values if available
-                if (storedPanicKey) {
-                    panicKeyInput.value = storedPanicKey;
+            // Set the input fields to the stored values if available
+            if (storedPanicKey) {
+                panicKeyInput.value = storedPanicKey;
+            }
+            if (storedWebsiteURL) {
+                websiteInput.value = storedWebsiteURL;
+            }
+        }
+
+        // Function to handle key press event
+        function handleKeyPress(event) {
+            var panicKey = panicKeyInput.value.trim().toLowerCase();
+
+            if (event.key === panicKey) {
+                var websiteURL = websiteInput.value.trim();
+
+                if (websiteURL !== '') {
+                    // Redirect to the entered URL
+                    window.location.href = websiteURL;
+
+                    // Store values in localStorage
+                    localStorage.setItem('panicKey', panicKey);
+                    localStorage.setItem('websiteURL', websiteURL);
                 }
-                if (storedWebsiteURL) {
-                    websiteInput.value = storedWebsiteURL;
-                }
-
-                document.addEventListener('keydown', function (event) {
-                    var panicKey = panicKeyInput.value.trim().toLowerCase();
-
-                    if (event.key === panicKey) {
-                        var websiteURL = websiteInput.value.trim();
-
-                        if (websiteURL !== '') {
-                            // Redirect to the entered URL
-                            window.location.href = websiteURL;
-
-                            localStorage.setItem('panicKey', panicKey);
-                            localStorage.setItem('websiteURL', websiteURL);
-                        }
-                    }
-                });
-            });
-        </script>
+            }
+        }
+    });
+</script>
