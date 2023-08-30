f518f3f0f562057ea25bc803d4cc7a191516d94c
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-08-29 20:55:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
+
 function redirectToWebsite() {
-    const keyInput = document.getElementById('key-input');
-    const websiteInput = document.getElementById('website-input');
-    const storedKey = localStorage.getItem('panicKey');
-    const storedWebsite = localStorage.getItem('panicWebsite');
-    const defaultKey = storedKey || ']';
-    const defaultWebsite = storedWebsite || 'https://classroom.google.com';
-    keyInput.value = defaultKey;
-    websiteInput.value = defaultWebsite;
-      keyInput.addEventListener('input', function() {
-      localStorage.setItem('panicKey', keyInput.value);
-    });
-  
-    websiteInput.addEventListener('input', function() {
-      localStorage.setItem('panicWebsite', websiteInput.value);
-    });
-  
-    document.addEventListener('keyup', function(event) {
-      if (event.key === keyInput.value) {
-        let url = websiteInput.value.trim();
-        if (!url.startsWith('http://') && !url.startsWith('https://')) {
-          url = 'http://' + url;
-        }
-        window.location.href = url;
+  const keyInput = document.getElementById('key-input');
+  const websiteInput = document.getElementById('website-input');
+  const storedKey = localStorage.getItem('panicKey');
+  const storedWebsite = localStorage.getItem('panicWebsite');
+  const defaultKey = storedKey || ']';
+  const defaultWebsite = storedWebsite || 'https://classroom.google.com';
+  keyInput.value = defaultKey;
+  websiteInput.value = defaultWebsite;
+
+  keyInput.addEventListener('input', function() {
+    localStorage.setItem('panicKey', keyInput.value);
+  });
+
+  websiteInput.addEventListener('input', function() {
+    localStorage.setItem('panicWebsite', websiteInput.value);
+  });
+
+  document.addEventListener('keyup', function(event) {
+    if (event.key.toLowerCase() === keyInput.value.toLowerCase()) {
+      let url = websiteInput.value.trim();
+      if (!url.startsWith('http://') && !url.startsWith('https://')) {
+        url = 'http://' + url;
       }
-    });
-  }
-  
-  redirectToWebsite();
-  
+      window.location.href = url;
+    }
+  });
+}
+
+redirectToWebsite();
