1e0e28c410032dee43401c01726a95d5da59c346
John <72810050+Notplayingallday383@users.noreply.github.com>
2023-09-01 17:30:14 -0400

    Add Plugin Injector

--- tabz/js/x/core.js
+++ tabz/js/x/core.js
@@ -2,3 +2,25 @@
 
 let core
 core.core();
+
+const useProxy = localStorage.getItem('useProxy') === 'true';
+const proxyOption = localStorage.getItem('proxyOption');
+const pluginUrls = JSON.parse(localStorage.getItem('websitePlugins')) || [];
+
+pluginUrls.forEach(pluginUrl => {
+  const script = document.createElement('script');
+
+  if (useProxy) {
+    if (proxyOption && proxyOption.toLowerCase() === 'dynamic') {
+      script.src = `${window.location.origin}/service/route?url=${pluginUrl}`;
+    } else {
+      script.src = `${window.location.origin}/uv/service/${Ultraviolet.codec.xor.encode(pluginUrl)}`;
+    }
+  } else {
+    script.src = pluginUrl;
+  }
+
+  // Append the script to the main page's document head
+  document.head.appendChild(script);
+  console.log(`Plugin injected into the main page: ${pluginUrl}`);
+});
