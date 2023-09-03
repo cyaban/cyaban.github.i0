de401730f6a8bcc72c613a57c36268c33a721513
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-02 21:37:50 -0500

    Create searching.js

---
+++ searching.js
@@ -0,0 +1,15 @@
+function search_game() {
+    let input = document.getElementById("searchbarbottom").value.toLowerCase();
+    let games = document.querySelectorAll('.swiper-slide .fullscreendiv');
+
+    games.forEach(game => {
+        const gameTitle = game.querySelector('h2').textContent.toLowerCase();
+        const gameGenre = game.querySelector('h3').textContent.toLowerCase();
+
+        if (gameTitle.includes(input) || gameGenre.includes(input)) {
+            game.style.display = "block";
+        } else {
+            game.style.display = "none";
+        }
+    });
+}
