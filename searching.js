32a2f3a9c8bde4960fb71b078759d7d4e3856319
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-04 07:53:24 -0500

    Update searching.js

--- searching.js
+++ searching.js
@@ -1,6 +1,5 @@
-// Create a new JavaScript module
-// mycode.js
-export function searchGames() {
+// Function to handle the search input
+function searchGames() {
     // Get the search input element
     var input = document.querySelector('.search-txt');
     // Get the game cards
@@ -9,22 +8,39 @@ export function searchGames() {
     // Convert the search input value to lowercase for case-insensitive search
     var searchTerm = input.value.toLowerCase();
 
+    // Determine the number of matching game cards
+    var matchingGameCards = [];
+
     // Loop through all game cards
     gameCards.forEach(function(card) {
         var title = card.querySelector('h2').textContent.toLowerCase(); // Get the game title
 
         // Check if the game title contains the search term
         if (title.includes(searchTerm)) {
-            card.style.display = 'block'; // Show the card if it matches the search term
-        } else {
-            card.style.display = 'none'; // Hide the card if it doesn't match
+            matchingGameCards.push(card);
         }
+
+        // Reset card style and swiper if not matching
+        card.style.display = 'none';
+        card.swiper.disable(); // Disable swiper for all cards initially
     });
+
+    // If there are 1 or 2 matching game cards, enable swiper for them
+    if (matchingGameCards.length === 1 || matchingGameCards.length === 2) {
+        matchingGameCards.forEach(function(card) {
+            card.style.display = 'block'; // Show the card
+            card.swiper.enable(); // Enable swiper for these cards
+        });
+    } else {
+        // For more than 2 matching game cards, keep swiper disabled
+        matchingGameCards.forEach(function(card) {
+            card.style.display = 'block'; // Show the card
+        });
+    }
 }
 
 // Add an event listener to the search input
 var searchInput = document.querySelector('.search-txt');
 searchInput.addEventListener('input', searchGames);
-
 
 
