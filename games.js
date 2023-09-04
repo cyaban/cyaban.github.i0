362e1020695e66528a436c7334fbbeffed1f74be
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-04 09:14:05 -0500

    Update games.js

--- games.js
+++ games.js
@@ -27,28 +27,62 @@ document.addEventListener('DOMContentLoaded', function () {
       },
     },
   });
+});
 
-  const menu = document.querySelector('.menu-icon');
-  const navbar = document.querySelector('.menu');
-  menu.onclick = () => {
-    navbar.classList.toggle('active');
-    menu.classList.toggle('move');
-  };
+// Function to handle the search input
+function searchGames() {
+  // Get the search input element
+  var input = document.querySelector('.search-txt');
+  // Get the game cards
+  var gameCards = document.querySelectorAll('.swiper-slide');
 
-  const favoriteButtons = document.querySelectorAll('.favorite-button');
-  favoriteButtons.forEach(button => {
-    button.addEventListener('click', addToFavorites);
+  // Convert the search input value to lowercase for case-insensitive search
+  var searchTerm = input.value.toLowerCase();
+
+  // Pause the Swiper autoplay while searching
+  swiper.autoplay.stop();
+
+  // Loop through all game cards
+  gameCards.forEach(function (card) {
+    var title = card.querySelector('h2').textContent.toLowerCase(); // Get the game title
+
+    // Check if the game title contains the search term
+    if (title.includes(searchTerm)) {
+      card.style.display = 'block'; // Show the card if it matches the search term
+    } else {
+      card.style.display = 'none'; // Hide the card if it doesn't match
+    }
   });
 
-  function addToFavorites(event) {
-    const gameId = event.target.dataset.gameId;
+  // Resume the Swiper autoplay after the search
+  swiper.autoplay.start();
+}
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', '/api/favorites', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({ gameId }));
-  }
+// Add an event listener to the search input
+var searchInput = document.querySelector('.search-txt');
+searchInput.addEventListener('input', searchGames);
+
+// Rest of your code for menu and favorite buttons
+const menu = document.querySelector('.menu-icon');
+const navbar = document.querySelector('.menu');
+menu.onclick = () => {
+  navbar.classList.toggle('active');
+  menu.classList.toggle('move');
+};
+
+const favoriteButtons = document.querySelectorAll('.favorite-button');
+favoriteButtons.forEach(button => {
+  button.addEventListener('click', addToFavorites);
 });
+
+function addToFavorites(event) {
+  const gameId = event.target.dataset.gameId;
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', '/api/favorites', true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({ gameId }));
+}
 
 
 
