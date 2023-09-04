1759b48115cce2d5f05485a018952ce4cf29c9f6
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-04 12:10:07 -0500

    Update games.js

--- games.js
+++ games.js
@@ -1,52 +1,4 @@
-let menu = document.querySelector('.menu-icon');
-let navbar = document.querySelector('.menu');
-menu.onclick = () => {
-  navbar.classList.toggle('active');
-  menu.classList.toggle('move');
-};
-
-var autoplayEnabled = true; // Flag to control autoplay
-
-var swiper = new Swiper(".trending-content", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-    },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 15,
-    },
-    1068: {
-      slidesPerView: 4,
-      spaceBetween: 20,
-    },
-  },
-});
-
-const favoriteButtons = document.querySelectorAll('.favorite-button');
-favoriteButtons.forEach(button => {
-  button.addEventListener('click', addToFavorites);
-});
-
-function addToFavorites(event) {
-  const gameId = event.target.dataset.gameId;
-
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', '/api/favorites', true);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(JSON.stringify({ gameId }));
-}
+// ...
 
 // Function to handle the search input
 function searchGames() {
@@ -57,6 +9,15 @@ function searchGames() {
 
   // Convert the search input value to lowercase for case-insensitive search
   var searchTerm = input.value.toLowerCase();
+
+  // Toggle autoplay based on search input
+  if (searchTerm.length > 0 && autoplayEnabled) {
+    swiper.autoplay.stop();
+    autoplayEnabled = false;
+  } else if (searchTerm.length === 0 && !autoplayEnabled) {
+    swiper.autoplay.start();
+    autoplayEnabled = true;
+  }
 
   // Loop through all game cards
   gameCards.forEach(function (card) {
@@ -69,17 +30,10 @@ function searchGames() {
       card.style.display = 'none'; // Hide the card if it doesn't match
     }
   });
-
-  // Toggle autoplay based on search input
-  if (searchTerm.length > 0 && autoplayEnabled) {
-    swiper.autoplay.stop();
-    autoplayEnabled = false;
-  } else if (searchTerm.length === 0 && !autoplayEnabled) {
-    swiper.autoplay.start();
-    autoplayEnabled = true;
-  }
 }
 
 // Add an event listener to the search input
 var searchInput = document.querySelector('.search-txt');
 searchInput.addEventListener('input', searchGames);
+
+// ...
