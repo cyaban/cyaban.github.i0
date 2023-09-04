384d5cfe32bf01c3ced7361d4d09aefce7c253af
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-04 16:50:43 -0500

    Update searching.js

--- searching.js
+++ searching.js
@@ -1,53 +1,3 @@
-let menu = document.querySelector('.menu-icon');
-let navbar = document.querySelector('.menu');
-menu.onclick = () => {
-  navbar.classList.toggle('active');
-  menu.classList.toggle('move');
-};
-
-var autoplayEnabled = true; // Flag to control autoplay
-
-var swiper = new Swiper(".trending-content", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-    },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 15,
-    },
-    1068: {
-      slidesPerView: 4,
-      spaceBetween: 20,
-    },
-  },
-});
-
-const favoriteButtons = document.querySelectorAll('.favorite-button');
-favoriteButtons.forEach(button => {
-  button.addEventListener('click', addToFavorites);
-});
-
-function addToFavorites(event) {
-  const gameId = event.target.dataset.gameId;
-
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', '/api/favorites', true);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(JSON.stringify({ gameId }));
-}
-
 // Function to handle the search input
 function searchGames() {
   // Get the search input element
@@ -59,12 +9,16 @@ function searchGames() {
   var searchTerm = input.value.toLowerCase();
 
   // Toggle autoplay based on search input
-  if (searchTerm.length > 0 && autoplayEnabled) {
-    swiper.autoplay.stop();
-    autoplayEnabled = false;
-  } else if (searchTerm.length === 0 && !autoplayEnabled) {
-    swiper.autoplay.start();
-    autoplayEnabled = true;
+  if (searchTerm.length > 0) {
+    if (autoplayEnabled) {
+      swiper.autoplay.stop();
+      autoplayEnabled = false;
+    }
+  } else {
+    if (!autoplayEnabled) {
+      swiper.autoplay.start();
+      autoplayEnabled = true;
+    }
   }
 
   // Loop through all game cards
@@ -83,6 +37,3 @@ function searchGames() {
 // Add an event listener to the search input
 var searchInput = document.querySelector('.search-txt');
 searchInput.addEventListener('input', searchGames);
-
-// Initialize search functionality and autoplay state
-searchGames();
