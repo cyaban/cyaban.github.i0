481360887c62b7a80b147d0d0642279863c138e8
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-04 16:54:28 -0500

    Update searching.js

--- searching.js
+++ searching.js
@@ -8,6 +8,18 @@ function searchGames() {
   // Convert the search input value to lowercase for case-insensitive search
   var searchTerm = input.value.toLowerCase();
 
+  // Loop through all game cards
+  gameCards.forEach(function (card) {
+    var title = card.querySelector('h2').textContent.toLowerCase(); // Get the game title
+
+    // Check if the game title contains the search term
+    if (title.includes(searchTerm) || searchTerm.length === 0) {
+      card.style.display = 'block'; // Show the card if it matches the search term or if the search term is empty
+    } else {
+      card.style.display = 'none'; // Hide the card if it doesn't match
+    }
+  });
+
   // Toggle autoplay based on search input
   if (searchTerm.length > 0) {
     if (autoplayEnabled) {
@@ -20,18 +32,6 @@ function searchGames() {
       autoplayEnabled = true;
     }
   }
-
-  // Loop through all game cards
-  gameCards.forEach(function (card) {
-    var title = card.querySelector('h2').textContent.toLowerCase(); // Get the game title
-
-    // Check if the game title contains the search term
-    if (title.includes(searchTerm) || searchTerm.length === 0) {
-      card.style.display = 'block'; // Show the card if it matches the search term or if the search term is empty
-    } else {
-      card.style.display = 'none'; // Hide the card if it doesn't match
-    }
-  });
 }
 
 // Add an event listener to the search input
