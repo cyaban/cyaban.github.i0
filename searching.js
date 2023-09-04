20c23b164a65b078e5d14b881b3ba9218a275d47
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-04 13:45:10 -0500

    Update searching.js

--- searching.js
+++ searching.js
@@ -1,52 +1,4 @@
-let menu = document.querySelector('.menu-icon');
-let navbar = document.querySelector('.menu');
-menu.onclick = () => {
-  navbar.classList.toggle('active');
-  menu.classList.toggle('move');
-};
-
-var autoplayEnabled = true; // Flag to control autoplay
-
-var swiper = new Swiper(".trending-content", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-    },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 15,
-    },
-    1068: {
-      slidesPerView: 4,
-      spaceBetween: 20,
-    },
-  },
-});
-
-const favoriteButtons = document.querySelectorAll('.favorite-button');
-favoriteButtons.forEach(button => {
-  button.addEventListener('click', addToFavorites);
-});
-
-function addToFavorites(event) {
-  const gameId = event.target.dataset.gameId;
-
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', '/api/favorites', true);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(JSON.stringify({ gameId }));
-}
+// ...
 
 // Function to handle the search input
 function searchGames() {
@@ -58,29 +10,38 @@ function searchGames() {
   // Convert the search input value to lowercase for case-insensitive search
   var searchTerm = input.value.toLowerCase();
 
-  // Toggle autoplay based on search input
-  if (searchTerm.length > 0 && autoplayEnabled) {
-    swiper.autoplay.stop();
-    autoplayEnabled = false;
-  } else if (searchTerm.length === 0 && !autoplayEnabled) {
-    swiper.autoplay.start();
-    autoplayEnabled = true;
-  }
-
   // Loop through all game cards
   gameCards.forEach(function (card) {
     var title = card.querySelector('h2').textContent.toLowerCase(); // Get the game title
 
     // Check if the game title contains the search term
-    if (title.includes(searchTerm) || searchTerm.length === 0) {
-      card.style.display = 'block'; // Show the card if it matches the search term or if the search term is empty
+    if (title.includes(searchTerm)) {
+      card.style.display = 'block'; // Show the card if it matches the search term
     } else {
       card.style.display = 'none'; // Hide the card if it doesn't match
     }
   });
+
+  // Check if the search input has any content to stop autoplay
+  if (searchTerm.trim() !== '') {
+    swiper.autoplay.stop();
+  } else {
+    swiper.autoplay.start(); // Resume autoplay if the search input is empty
+  }
 }
 
-// Add an event listener to the search input
-var searchInput = document.querySelector('.search-txt');
-searchInput.addEventListener('input', searchGames);
+// ...
+
+// Pause autoplay when the search input is focused
+searchInput.addEventListener('focus', function () {
+  swiper.autoplay.stop();
+});
+
+// Resume autoplay when the search input is blurred
+searchInput.addEventListener('blur', function () {
+  swiper.autoplay.start();
+});
+
+// ...
+
 
