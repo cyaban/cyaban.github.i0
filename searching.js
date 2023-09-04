65e7dfbcd6ac55b131d4e20564a4feaaa232e413
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-04 16:42:12 -0500

    Update searching.js

--- searching.js
+++ searching.js
@@ -1,91 +1,34 @@
-var swiper = null; // Initialize swiper as null
-
 // Function to handle the search input
 function searchGames() {
-  // ... (Your existing search function code)
-  // ...
+    // Get the search input element
+    var input = document.querySelector('.search-txt');
+    // Get the game cards
+    var gameCards = document.querySelectorAll('.swiper-slide');
+    var swiper = document.querySelector(".trending-content").swiper; // Get the Swiper instance
 
-// Function to handle the search input
-function searchGames() {
-  // Get the search input element
-  var input = document.querySelector('.search-txt');
-  // Get the game cards
-  var gameCards = document.querySelectorAll('.swiper-slide');
+    // Convert the search input value to lowercase for case-insensitive search
+    var searchTerm = input.value.toLowerCase();
 
-  // Convert the search input value to lowercase for case-insensitive search
-  var searchTerm = input.value.toLowerCase();
+    // Loop through all game cards
+    gameCards.forEach(function(card) {
+        var title = card.querySelector('h2').textContent.toLowerCase(); // Get the game title
 
-  // Loop through all game cards
-  gameCards.forEach(function (card) {
-    var title = card.querySelector('h2').textContent.toLowerCase(); // Get the game title
+        // Check if the game title contains the search term
+        if (title.includes(searchTerm)) {
+            card.style.display = 'block'; // Show the card if it matches the search term
+        } else {
+            card.style.display = 'none'; // Hide the card if it doesn't match
+        }
+    });
 
-    // Check if the game title contains the search term
-    if (title.includes(searchTerm)) {
-      card.style.display = 'block'; // Show the card if it matches the search term
-    } else {
-      card.style.display = 'none'; // Hide the card if it doesn't match
+    // Check if the Swiper instance exists and the search input is not empty
+    if (swiper && searchTerm.trim() !== '') {
+        swiper.autoplay.stop(); // Stop Swiper autoplay
+    } else if (swiper) {
+        swiper.autoplay.start(); // Start Swiper autoplay if input is empty
     }
-  });
-
-  // Check if the search input has any content to stop autoplay
-  if (searchTerm.trim() !== '') {
-    swiper.autoplay.stop();
-  } else {
-    swiper.autoplay.start(); // Resume autoplay if the search input is empty
-  }
 }
 
-// ...
-
-// Pause autoplay when the search input is focused
-searchInput.addEventListener('focus', function () {
-  swiper.autoplay.stop();
-});
-
-// Resume autoplay when the search input is blurred
-searchInput.addEventListener('blur', function () {
-  swiper.autoplay.start();
-});
-
-// ...
-
-
-
-  // Check if swiper is initialized to stop/start autoplay
-  if (swiper !== null) {
-    if (searchTerm.trim() !== '') {
-      swiper.autoplay.stop();
-    } else {
-      swiper.autoplay.start();
-    }
-  }
-}
-
-// Pause autoplay when the search input is focused
-searchInput.addEventListener('focus', function () {
-  if (swiper !== null) {
-    swiper.autoplay.stop();
-  }
-});
-
-// Resume autoplay when the search input is blurred
-searchInput.addEventListener('blur', function () {
-  if (swiper !== null) {
-    swiper.autoplay.start();
-  }
-});
-
-// ... (Other parts of your code)
-
-// Initialize swiper when needed (you can place this in an event handler, for example)
-function initializeSwiper() {
-  swiper = new Swiper('.swiper-container', {
-    // swiper.js configuration options
-  });
-}
-
-// Call initializeSwiper when you want to start swiper.js
-// For example, after the page loads or when a specific event occurs
-initializeSwiper();
-
-
+// Add an event listener to the search input
+var searchInput = document.querySelector('.search-txt');
+searchInput.addEventListener('input', searchGames);
