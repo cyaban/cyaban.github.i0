d9c6a3b9a5da84ed717d1785bd15f11924bbc8c7
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-04 21:17:26 -0500

    Update themes.js

--- themes.js
+++ themes.js
@@ -1,31 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var saveButton = document.getElementById("save-button");
-  var resetButton = document.getElementById("reset-button");
-  var allCards = document.querySelector(".allcards");
-
-  // Load the saved background image from localStorage
-  var savedBackgroundImage = localStorage.getItem("backgroundImage");
-  if (savedBackgroundImage) {
-    allCards.style.backgroundImage = "url('" + savedBackgroundImage + "')";
-  }
-
-  // Save button click event
-  saveButton.addEventListener("click", function() {
-    var backgroundInput = document.getElementById("background-input");
-    var imageURL = backgroundInput.value;
-
-    if (imageURL !== "") {
-      localStorage.setItem("backgroundImage", imageURL);
-      allCards.style.backgroundImage = "url('" + imageURL + "')";
-      backgroundInput.value = "";
-    } else {
-      alert("Please enter a valid image URL.");
-    }
-  });
-
-  // Reset button click event
-  resetButton.addEventListener("click", function() {
-    localStorage.removeItem("backgroundImage");
-    allCards.style.backgroundImage = "none";
-  });
-});
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var saveButton = document.getElementById("save-button");
+        var resetButton = document.getElementById("reset-button");
+        var allCards = document.querySelector(".allcards");
+    
+        // Load the saved background image from localStorage
+        var savedBackgroundImage = localStorage.getItem("backgroundImage");
+        if (savedBackgroundImage) {
+            allCards.style.backgroundImage = "url('" + savedBackgroundImage + "')";
+        }
+    
+        // Save button click event
+        saveButton.addEventListener("click", function() {
+            var backgroundInput = document.getElementById("background-input");
+            var imageURL = backgroundInput.value;
+    
+            if (imageURL.trim() !== "") {
+                localStorage.setItem("backgroundImage", imageURL);
+                allCards.style.backgroundImage = "url('" + imageURL + "')";
+                backgroundInput.value = "";
+            } else {
+                alert("Please enter a valid image URL.");
+            }
+        });
+    
+        // Reset button click event
+        resetButton.addEventListener("click", function() {
+            localStorage.removeItem("backgroundImage");
+            allCards.style.backgroundImage = "none";
+        });
+    });
+</script>
