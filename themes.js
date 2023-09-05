5cbb89e7507503537bbb737702b1310bded2c7a6
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-04 21:14:28 -0500

    Update themes.js

--- themes.js
+++ themes.js
@@ -1,26 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
   var saveButton = document.getElementById("save-button");
+  var resetButton = document.getElementById("reset-button");
+  var allCards = document.querySelector(".allcards");
+
+  // Load the saved background image from localStorage
+  var savedBackgroundImage = localStorage.getItem("backgroundImage");
+  if (savedBackgroundImage) {
+    allCards.style.backgroundImage = "url('" + savedBackgroundImage + "')";
+  }
+
+  // Save button click event
   saveButton.addEventListener("click", function() {
     var backgroundInput = document.getElementById("background-input");
     var imageURL = backgroundInput.value;
 
     if (imageURL !== "") {
       localStorage.setItem("backgroundImage", imageURL);
-      document.querySelector(".allcards").style.backgroundImage = "url('" + imageURL + "')";
+      allCards.style.backgroundImage = "url('" + imageURL + "')";
       backgroundInput.value = "";
     } else {
       alert("Please enter a valid image URL.");
     }
   });
 
-  var resetButton = document.getElementById("reset-button");
+  // Reset button click event
   resetButton.addEventListener("click", function() {
     localStorage.removeItem("backgroundImage");
-    document.querySelector(".allcards").style.backgroundImage = "none";
+    allCards.style.backgroundImage = "none";
   });
-
-  var savedBackgroundImage = localStorage.getItem("backgroundImage");
-  if (savedBackgroundImage) {
-    document.querySelector(".allcards").style.backgroundImage = "url('" + savedBackgroundImage + "')";
-  }
 });
