4056e0efcd3c1f06291d17e06f79a37024d0bf94
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-04 21:02:41 -0500

    Update themes.js

--- themes.js
+++ themes.js
@@ -1,23 +1,21 @@
-// Function to save background image URL to local storage
-document.getElementById('save-button').addEventListener('click', function () {
-    const backgroundImageURL = document.getElementById('background-input').value;
-    localStorage.setItem('customBackground', backgroundImageURL);
-    // Apply the background image to your theme here
-    document.body.style.backgroundImage = `url('${backgroundImageURL}')`;
-});
+const cardContainer = document.getElementById('card-container');
+        const backgroundInput = document.getElementById('background-input');
+        const saveButton = document.getElementById('save-button');
+        const resetButton = document.getElementById('reset-button');
 
-// Function to reset background image and clear local storage
-document.getElementById('reset-button').addEventListener('click', function () {
-    localStorage.removeItem('customBackground');
-    // Reset the background image to the default here
-    document.body.style.backgroundImage = 'none';
-    document.getElementById('background-input').value = '';
-});
+        // Function to set the background of the card container
+        function setBackground(backgroundURL) {
+            cardContainer.style.backgroundImage = `url(${backgroundURL})`;
+        }
 
-// Check if there's a saved background image in local storage
-const savedBackground = localStorage.getItem('customBackground');
-if (savedBackground) {
-    // Apply the saved background image to your theme here
-    document.body.style.backgroundImage = `url('${savedBackground}')`;
-    document.getElementById('background-input').value = savedBackground;
-}
+        // Event listener for the Save button
+        saveButton.addEventListener('click', () => {
+            const imageURL = backgroundInput.value;
+            setBackground(imageURL);
+        });
+
+        // Event listener for the Reset button
+        resetButton.addEventListener('click', () => {
+            backgroundInput.value = ''; // Clear the input field
+            cardContainer.style.backgroundImage = 'none'; // Reset background
+        });
