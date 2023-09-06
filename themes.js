4af4173f5f4cc33c7ea926d2f094b15605f8eba6
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-06 06:14:48 -0500

    Update themes.js

--- themes.js
+++ themes.js
@@ -1,33 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var saveButton = document.getElementById("save-button");
-  var resetButton = document.getElementById("reset-button");
-  var backgroundInput = document.getElementById("background-input");
-  var cardContainer = document.querySelector(".card-container");
 
-  // Function to set the background image and save it to localStorage
-  function setBackground(imageURL) {
-    if (imageURL !== "") {
-      localStorage.setItem("backgroundImage", imageURL);
-      document.body.style.backgroundImage = "url('" + imageURL + "')";
-      backgroundInput.value = "";
-    }
-  }
+        document.addEventListener("DOMContentLoaded", function() {
+            var saveButton = document.getElementById("save-button");
+            saveButton.addEventListener("click", function() {
+                var backgroundInput = document.getElementById("background-input");
+                var imageURL = backgroundInput.value;
 
-  // Event listener for the Save button
-  saveButton.addEventListener("click", function() {
-    var imageURL = backgroundInput.value;
-    setBackground(imageURL);
-  });
+                if (imageURL !== "") {
+                    localStorage.setItem("backgroundImage", imageURL);
+                    document.body.style.backgroundImage = "url('" + imageURL + "')";
+                    backgroundInput.value = "";
+                } else {
+                    // Handle the case when the input is empty
+                }
+            });
 
-  // Event listener for the Reset button
-  resetButton.addEventListener("click", function() {
-    localStorage.removeItem("backgroundImage");
-    document.body.style.backgroundImage = "url('default-background.jpg')";
-  });
+            var resetButton = document.getElementById("reset-button");
+            resetButton.addEventListener("click", function() {
+                localStorage.removeItem("backgroundImage");
+                document.body.style.backgroundImage = "url('default-background.jpg')";
+            });
 
-  // Check if there's a saved background image and set it
-  var savedBackgroundImage = localStorage.getItem("backgroundImage");
-  if (savedBackgroundImage) {
-    setBackground(savedBackgroundImage);
-  }
-});
+            var savedBackgroundImage = localStorage.getItem("backgroundImage");
+            if (savedBackgroundImage) {
+                document.body.style.backgroundImage = "url('" + savedBackgroundImage + "')";
+            }
+        });
+
+
