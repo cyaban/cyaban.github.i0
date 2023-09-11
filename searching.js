434359d41edbc490fec2daeff48cdaa6e43e2d81
Alphabet <118471310+DEVAbdelToom@users.noreply.github.com>
2023-09-11 19:16:25 -0400

    Update searching.js Backup added

--- searching.js
+++ searching.js
@@ -1,5 +1,6 @@
+// Function to handle the search input
 function searchGames() {
-    // Get the search input element 
+    // Get the search input element
     var input = document.querySelector('.search-txt');
     // Get the game cards
     var gameCards = document.querySelectorAll('.swiper-slide');
@@ -9,7 +10,7 @@ function searchGames() {
     // Convert the search input value to lowercase for case-insensitive search
     var searchTerm = input.value.toLowerCase();
 
-    var resultsFound = false;
+    var resultsFound = false; // Flag to track if any results were found
 
     // Loop through all game cards
     gameCards.forEach(function(card) {
@@ -50,65 +51,11 @@ function searchGames() {
             link.style.display = 'block';
         }
     });
-
-    // Hide the pagination if there is text in the search input and there are no results
-    if (input.value.length > 0 && !resultsFound) {
-        pagination.style.display = 'none';
-    } else {
-        pagination.style.display = 'block';
-    }
 }
 
 // Add an event listener to the search input
 var searchInput = document.querySelector('.search-txt');
 searchInput.addEventListener('input', searchGames);
-
-// Add an event listener to the search input
-var searchInput = document.querySelector('.search-txt');
-searchInput.addEventListener('input', searchGames);// Flag to track if any results were found
-
-    // Loop through all game cards
-    gameCards.forEach(function(card) {
-        var title = card.querySelector('h2').textContent.toLowerCase(); // Get the game title
-
-        // Check if the game title contains the search term
-        if (title.includes(searchTerm)) {
-            card.style.display = 'block'; // Show the card if it matches the search term
-            resultsFound = true; // Set the flag to true since a result was found
-        } else {
-            card.style.display = 'none'; // Hide the card if it doesn't match
-        }
-    });
-
-    // Hide or show the "No results found" message based on the flag
-    if (resultsFound) {
-        noResultsMessage.style.display = 'none'; // Hide the message if results were found
-    } else {
-        noResultsMessage.style.display = 'block'; // Show the message if no results were found
-    }
-
-    // Hide or show the trending sections based on the flag
-    var trendingSections = document.querySelectorAll('.trending.container');
-    trendingSections.forEach(function(section) {
-        if (!resultsFound) {
-            section.style.display = 'none'; // Hide the section if no results were found
-        } else {
-            section.style.display = 'block'; // Show the section if results were found
-        }
-    });
-
-    // Hide the <a> elements with class "view-more" if no results were found
-    var viewMoreLinks = document.querySelectorAll('.view-more a');
-    viewMoreLinks.forEach(function(link) {
-        if (!resultsFound) {
-            link.style.display = 'none';
-        } else {
-            link.style.display = 'block';
-        }
-    });
-}
-
-
 // Add an event listener to the search input
 var searchInput = document.querySelector('.search-txt');
 searchInput.addEventListener('input', searchGames);
