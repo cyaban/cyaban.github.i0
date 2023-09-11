8c00829a7b76ce0aff88d239e2690d912d36f17f
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-11 12:41:19 -0500

    Update searching.js

--- searching.js
+++ searching.js
@@ -4,12 +4,8 @@ function searchGames() {
     var input = document.querySelector('.search-txt');
     // Get the game cards
     var gameCards = document.querySelectorAll('.swiper-slide');
-    // Get the <h2> element
-    var h2Element = document.querySelector('.io-games h2');
-    // Get the <i> element with class "fa-star"
-    var starIcon = document.querySelector('.io-games .fa-star');
-    // Get the <h5> element
-    var h5Element = document.querySelector('.io-games .view-more');
+    // Get the entire "trending" section
+    var trendingSection = document.querySelector('.trending');
 
     // Convert the search input value to lowercase for case-insensitive search
     var searchTerm = input.value.toLowerCase();
@@ -29,15 +25,11 @@ function searchGames() {
         }
     });
 
-    // Hide or show the <h2> element, <i> element, and <h5> element based on the flag
+    // Hide or show the entire "trending" section based on the flag
     if (resultsFound) {
-        h2Element.style.display = 'block'; // Show the <h2> element if results were found
-        starIcon.style.display = 'block'; // Show the <i> element with class "fa-star" if results were found
-        h5Element.style.display = 'block'; // Show the <h5> element if results were found
+        trendingSection.style.display = 'block'; // Show the section if results were found
     } else {
-        h2Element.style.display = 'none'; // Hide the <h2> element if no results were found
-        starIcon.style.display = 'none'; // Hide the <i> element with class "fa-star" if no results were found
-        h5Element.style.display = 'none'; // Hide the <h5> element if no results were found
+        trendingSection.style.display = 'none'; // Hide the section if no results were found
     }
 }
 
