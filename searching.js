b684760751e484b8b5858e31ea62705b4519bb19
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-11 09:16:46 -0500

    Update searching.js

--- searching.js
+++ searching.js
@@ -6,19 +6,20 @@ function searchGames() {
     var gameCards = document.querySelectorAll('.swiper-slide');
     // Get the element where you want to display the "No results found" message
     var noResultsMessage = document.querySelector('.no-results-message');
+    
     // Get the elements you want to hide/show
     var ioGamesHeader = document.querySelector('h2');
     var viewMoreLink = document.querySelector('.view-more');
-
+    
     // Convert the search input value to lowercase for case-insensitive search
     var searchTerm = input.value.toLowerCase();
-
+    
     var resultsFound = false; // Flag to track if any results were found
-
+    
     // Loop through all game cards
     gameCards.forEach(function(card) {
         var title = card.querySelector('h2').textContent.toLowerCase(); // Get the game title
-
+        
         // Check if the game title contains the search term
         if (title.includes(searchTerm)) {
             card.style.display = 'block'; // Show the card if it matches the search term
@@ -27,16 +28,21 @@ function searchGames() {
             card.style.display = 'none'; // Hide the card if it doesn't match
         }
     });
-
+    
     // Hide or show the "No results found" message based on the flag
     if (resultsFound) {
         noResultsMessage.style.display = 'none'; // Hide the message if results were found
-        ioGamesHeader.style.display = 'block'; // Show the "IO Games" header
-        viewMoreLink.style.display = 'block'; // Show the "View more" link
     } else {
         noResultsMessage.style.display = 'block'; // Show the message if no results were found
-        ioGamesHeader.style.display = 'none'; // Hide the "IO Games" header
-        viewMoreLink.style.display = 'none'; // Hide the "View more" link
+    }
+    
+    // Hide the "IO Games" header and "View more" link if no results were found
+    if (!resultsFound) {
+        ioGamesHeader.style.display = 'none';
+        viewMoreLink.style.display = 'none';
+    } else {
+        ioGamesHeader.style.display = 'block';
+        viewMoreLink.style.display = 'block';
     }
 }
 
@@ -46,3 +52,4 @@ searchInput.addEventListener('input', searchGames);
 
 // Initial search to hide "IO Games" and "View more" if no results
 searchGames();
+
