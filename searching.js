b398a7f7fe46c01752b7f27049c8c2945d0e9450
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-10 22:17:35 -0500

    Update searching.js

--- searching.js
+++ searching.js
@@ -1,3 +1,4 @@
+// Function to handle the search input
 function searchGames() {
     // Get the search input element
     var input = document.querySelector('.search-txt');
@@ -5,61 +6,58 @@ function searchGames() {
     var gameCards = document.querySelectorAll('.swiper-slide');
     // Get the element where you want to display the "No results found" message
     var noResultsMessage = document.querySelector('.no-results-message');
-    // Get the specific <h2> element you want to hide when no results are found
-    var ioGamesHeader = document.querySelector('.swiper-slide h2');
-    
+
     // Convert the search input value to lowercase for case-insensitive search
     var searchTerm = input.value.toLowerCase();
-    
+
     var resultsFound = false; // Flag to track if any results were found
-    
+
     // Loop through all game cards
     gameCards.forEach(function(card) {
         var title = card.querySelector('h2').textContent.toLowerCase(); // Get the game title
-        
+
         // Check if the game title contains the search term
         if (title.includes(searchTerm)) {
             card.style.display = 'block'; // Show the card if it matches the search term
+            card.querySelector('h2').style.display = 'block'; // Show the h2 element
             resultsFound = true; // Set the flag to true since a result was found
         } else {
             card.style.display = 'none'; // Hide the card if it doesn't match
+            card.querySelector('h2').style.display = 'none'; // Hide the h2 element
         }
     });
-    
+
     // Hide or show the "No results found" message based on the flag
-    noResultsMessage.style.display = resultsFound ? 'none' : 'block';
-    
-    // Remove the <h2> element when no results are found
-    if (!resultsFound) {
-        ioGamesHeader.parentNode.removeChild(ioGamesHeader);
+    if (resultsFound) {
+        noResultsMessage.style.display = 'none'; // Hide the message if results were found
+    } else {
+        noResultsMessage.style.display = 'block'; // Show the message if no results were found
     }
-    
-    // Hide or show other elements based on the flag
+
+    // Hide the <a> elements with class "view-more" if no results were found
     var viewMoreLinks = document.querySelectorAll('.view-more a');
+    viewMoreLinks.forEach(function(link) {
+        if (!resultsFound) {
+            link.style.display = 'none';
+        } else {
+            link.style.display = 'block';
+        }
+    });
+
+    // Hide the <a> elements with class "fa-star" and "fa-angles-right" if no results were found
     var starLinks = document.querySelectorAll('.fa-star');
     var anglesRightLinks = document.querySelectorAll('.fa-angles-right');
-    
     if (!resultsFound) {
-        viewMoreLinks.forEach(function(link) {
-            link.style.display = 'none';
-        });
-        
         starLinks.forEach(function(starLink) {
             starLink.style.display = 'none';
         });
-        
         anglesRightLinks.forEach(function(anglesRightLink) {
             anglesRightLink.style.display = 'none';
         });
     } else {
-        viewMoreLinks.forEach(function(link) {
-            link.style.display = 'block';
-        });
-        
         starLinks.forEach(function(starLink) {
             starLink.style.display = 'block';
         });
-        
         anglesRightLinks.forEach(function(anglesRightLink) {
             anglesRightLink.style.display = 'block';
         });
