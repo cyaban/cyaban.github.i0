449dd6e61bebd67809a7d7d005bb38f53617fdd6
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-11 07:38:24 -0500

    Update searching.js

--- searching.js
+++ searching.js
@@ -1,3 +1,4 @@
+// Function to handle the search input
 function searchGames() {
     // Get the search input element
     var input = document.querySelector('.search-txt');
@@ -12,12 +13,11 @@ function searchGames() {
     var resultsFound = false; // Flag to track if any results were found
 
     // Loop through all game cards
-    gameCards.forEach(function (card) {
-        var titleElement = card.querySelector('h2'); // Get the h2 element
-        var titleText = titleElement.textContent.toLowerCase(); // Get the game title text
+    gameCards.forEach(function(card) {
+        var title = card.querySelector('h2').textContent.toLowerCase(); // Get the game title
 
-        // Check if the game title text contains the search term
-        if (titleText.includes(searchTerm)) {
+        // Check if the game title contains the search term
+        if (title.includes(searchTerm)) {
             card.style.display = 'block'; // Show the card if it matches the search term
             resultsFound = true; // Set the flag to true since a result was found
         } else {
@@ -25,12 +25,25 @@ function searchGames() {
         }
     });
 
-    // Display or hide the "No results found" message based on the flag
+    // Hide or show the "No results found" message based on the flag
     if (resultsFound) {
         noResultsMessage.style.display = 'none'; // Hide the message if results were found
     } else {
         noResultsMessage.style.display = 'block'; // Show the message if no results were found
     }
+
+    // Hide the <a> elements with class "view-more" if no results were found
+    var viewMoreLinks = document.querySelectorAll('.view-more a');
+    viewMoreLinks.forEach(function(link) {
+        if (!resultsFound) {
+            link.style.display = 'none';
+        } else {
+            link.style.display = 'block';
+        }
+    });
 }
 
+// Add an event listener to the search input
+var searchInput = document.querySelector('.search-txt');
+searchInput.addEventListener('input', searchGames);
 
