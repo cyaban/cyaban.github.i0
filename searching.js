f5ef5327f052414443f54cefe24fe08c5be38c07
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-10 20:38:34 -0500

    Update searching.js

--- searching.js
+++ searching.js
@@ -4,9 +4,13 @@ function searchGames() {
     var input = document.querySelector('.search-txt');
     // Get the game cards
     var gameCards = document.querySelectorAll('.swiper-slide');
+    // Get the element where you want to display the "No results found" message
+    var noResultsMessage = document.querySelector('.no-results-message');
 
     // Convert the search input value to lowercase for case-insensitive search
     var searchTerm = input.value.toLowerCase();
+    
+    var resultsFound = false; // Flag to track if any results were found
 
     // Loop through all game cards
     gameCards.forEach(function(card) {
@@ -15,10 +19,18 @@ function searchGames() {
         // Check if the game title contains the search term
         if (title.includes(searchTerm)) {
             card.style.display = 'block'; // Show the card if it matches the search term
+            resultsFound = true; // Set the flag to true since a result was found
         } else {
             card.style.display = 'none'; // Hide the card if it doesn't match
         }
     });
+
+    // Display or hide the "No results found" message based on the flag
+    if (resultsFound) {
+        noResultsMessage.style.display = 'none'; // Hide the message if results were found
+    } else {
+        noResultsMessage.style.display = 'block'; // Show the message if no results were found
+    }
 }
 
 // Add an event listener to the search input
