cc9d32d7334d682c3ac48a4dc967a72e42ef5e8b
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-11 20:05:55 -0500

    Update searching2.js

--- searching2.js
+++ searching2.js
@@ -22,6 +22,12 @@ function searchGames() {
         } else {
             card.style.display = 'none'; // Hide the card if it doesn't match
         }
+        
+        // Check if the card contains an <a> element with href="io.html" and hide it if necessary
+        var ioLink = card.querySelector('a[href="io.html"]');
+        if (ioLink) {
+            ioLink.style.display = 'none';
+        }
     });
 
     // Hide or show the "No results found" message based on the flag
@@ -29,17 +35,35 @@ function searchGames() {
         noResultsMessage.style.display = 'none'; // Hide the message if results were found
     } else {
         noResultsMessage.style.display = 'block'; // Show the message if no results were found
-
-        // Hide the <a> and <h2> elements within the trending section
-        var trendingSection = document.querySelector('.trending.container#trending');
-        var elementsToHide = trendingSection.querySelectorAll('a, h2');
-        
-        elementsToHide.forEach(function(element) {
-            element.style.display = 'none';
-        });
     }
 
-    // ... (rest of the code remains the same)
+    // Hide or show the trending sections based on the flag
+    var trendingSections = document.querySelectorAll('.trending.container');
+    trendingSections.forEach(function(section) {
+        if (!resultsFound) {
+            section.style.display = 'none'; // Hide the section if no results were found
+        } else {
+            section.style.display = 'block'; // Show the section if results were found
+        }
+    });
+
+    // Hide the <a> elements with class "view-more" if no results were found
+    var viewMoreLinks = document.querySelectorAll('.view-more a');
+    viewMoreLinks.forEach(function(link) {
+        if (!resultsFound) {
+            link.style.display = 'none';
+        } else {
+            link.style.display = 'block';
+        }
+    });
+
+    // Hide the pagination if there is text in the search input
+    var pagination = document.querySelector('.pagination');
+    if (input.value.length > 0) {
+        pagination.style.display = 'none';
+    } else {
+        pagination.style.display = 'block';
+    }
 }
 
 // Add an event listener to the search input
