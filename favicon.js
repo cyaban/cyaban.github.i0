2dab50666c413cf34173113357fe41957b4a09e1
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-13 11:10:39 -0500

    Update favicon.js

--- favicon.js
+++ favicon.js
@@ -26,12 +26,38 @@ function resetTab() {
     setFavicon("default-favicon.ico"); // Reset the favicon
 }
 
+// Function to set mobile favicon
+function setMobileFavicon(iconUrl) {
+    const mobileFaviconLink = document.createElement("link");
+    mobileFaviconLink.rel = "apple-touch-icon";
+    mobileFaviconLink.href = iconUrl;
+
+    // Find the existing mobile favicon link and remove it
+    const existingMobileFavicon = document.querySelector("link[rel='apple-touch-icon']");
+    if (existingMobileFavicon) {
+        existingMobileFavicon.remove();
+    }
+
+    // Append the new mobile favicon link to the head of the document
+    document.head.appendChild(mobileFaviconLink);
+}
+
 // Add event listeners
-document.getElementById("icon").addEventListener("keydown", function (event) {
+const iconInput = document.getElementById("icon");
+iconInput.addEventListener("keydown", function (event) {
     if (event.keyCode === 13) { // Check if Enter key was pressed
         event.preventDefault(); // Prevent the default Enter key action (form submission)
         setFavicon(this.value.trim());
+        setMobileFavicon(this.value.trim());
     }
 });
 
+iconInput.addEventListener("change", function () {
+    setFavicon(this.value.trim());
+    setMobileFavicon(this.value.trim());
+});
+
 document.getElementById("resetBtn").addEventListener("click", resetTab);
+
+// Rest of your favicon cloaker code
+// ...
