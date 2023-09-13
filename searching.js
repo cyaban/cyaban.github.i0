a61ed1d1a8cb6f8b4783409c85fd64f608452ebe
Alphabet <118471310+DEVAbdelToom@users.noreply.github.com>
2023-09-13 13:14:15 -0400

    Update searching.js

--- searching.js
+++ searching.js
@@ -1,3 +1,9 @@
+Get the search input element
+var searchInput = document.querySelector('.search-txt');
+
+// Add an event listener to the search input
+searchInput.addEventListener('keydown', searchGames);
+
 function searchGames() {
     // Get the search input element
     var input = document.querySelector('.search-txt');
@@ -29,34 +35,6 @@ function searchGames() {
         noResultsMessage.style.display = 'none'; // Hide the message if results were found
     } else {
         noResultsMessage.style.display = 'block'; // Show the message if no results were found
-    }
-
-    // Hide or show the trending sections based on the flag
-    var trendingSections = document.querySelectorAll('.trending.container');
-    trendingSections.forEach(function(section) {
-        if (!resultsFound) {
-            section.style.display = 'none'; // Hide the section if no results were found
-        } else {
-            section.style.display = 'block'; // Show the section if results were found
-        }
-    });
-
-    // Hide the <a> elements with class "view-more" if no results were found
-    var viewMoreLinks = document.querySelectorAll('.view-more a');
-    viewMoreLinks.forEach(function(link) {
-        if (!resultsFound) {
-            link.style.display = 'none';
-        } else {
-            link.style.display = 'block';
-        }
-    });
-
-    // Hide the pagination if there is text in the search input
-    var pagination = document.querySelector('.pagination');
-    if (input.value.length > 0) {
-        pagination.style.display = 'none';
-    } else {
-        pagination.style.display = 'block';
     }
 
     // Hide the copyright container if there is text in the search input
