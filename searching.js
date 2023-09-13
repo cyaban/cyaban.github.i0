823d68ca1542c3a494495e0b6cad1bc9688a00aa
Alphabet <118471310+DEVAbdelToom@users.noreply.github.com>
2023-09-13 13:28:22 -0400

    Update searching.js

--- searching.js
+++ searching.js
@@ -7,59 +7,58 @@ function searchGames() {
     var noResultsMessage = document.querySelector('.no-results-message');
 
     // Convert the search input value to lowercase for case-insensitive search
-    var searchTerm = input.value.toLowerCase();
+// Convert the search input value to lowercase for case-insensitive search
+var searchTerm = input.value.toLowerCase();
 
-    var resultsFound = false; // Flag to track if any results were found
+var resultsFound = false; // Flag to track if any results were found
 
-    // Loop through all game cards
-    gameCards.forEach(function(card) {
-        var title = card.querySelector('h2').textContent.toLowerCase(); // Get the game title
+// Loop through all game cards
+gameCards.forEach(function(card) {
+    var title = card.querySelector('h2').textContent.toLowerCase(); // Get the game title
 
-        // Check if the game title contains the search term
-        if (title.includes(searchTerm)) {
-            card.style.display = 'block'; // Show the card if it matches the search term
-            resultsFound = true; // Set the flag to true since a result was found
-        } else {
-            card.style.display = 'none'; // Hide the card if it doesn't match
-        }
-    });
-
-    // Hide or show the "No results found" message based on the flag
-    if (resultsFound) {
-        noResultsMessage.style.display = 'none'; // Hide the message if results were found
+    // Check if the game title contains the search term
+    if (title.includes(searchTerm)) {
+        card.style.display = 'block'; // Show the card if it matches the search term
+        resultsFound = true; // Set the flag to true since a result was found
     } else {
-        noResultsMessage.style.display = 'block'; // Show the message if no results were found
+        card.style.display = 'none'; // Hide the card if it doesn't match
     }
+});
 
-    // Hide or show the trending sections based on the flag
-    var trendingSections = document.querySelectorAll('.trending.container');
-    trendingSections.forEach(function(section) {
-        if (!resultsFound) {
-            section.style.display = 'none'; // Hide the section if no results were found
-        } else {
-            section.style.display = 'block'; // Show the section if results were found
-        }
-    });
-
-    // Hide the <a> elements with class "view-more" if no results were found
-    var viewMoreLinks = document.querySelectorAll('.view-more a');
-    viewMoreLinks.forEach(function(link) {
-        if (!resultsFound) {
-            link.style.display = 'none';
-        } else {
-            link.style.display = 'block';
-        }
-    });
-
-    // Hide the pagination if there is text in the search input
-    var pagination = document.querySelector('.pagination');
-    if (input.value.length > 0) {
-        pagination.style.display = 'none';
-    } else {
-        pagination.style.display = 'block';
-    }
+// Hide or show the "No results found" message based on the flag
+if (resultsFound) {
+    noResultsMessage.style.display = 'none'; // Hide the message if results were found
+} else {
+    noResultsMessage.style.display = 'block'; // Show the message if no results were found
 }
 
-// Add an event listener to the search input
-var searchInput = document.querySelector('.search-txt');
-searchInput.addEventListener('input', searchGames);
+// Hide or show the trending sections based on the flag
+var trendingSections = document.querySelectorAll('.trending.container');
+trendingSections.forEach(function(section) {
+    if (!resultsFound) {
+        section.style.display = 'none'; // Hide the section if no results were found
+    } else {
+        section.style.display = 'block'; // Show the section if results were found
+    }
+});
+
+// Hide the <a> elements with class "view-more" if no results were found
+var viewMoreLinks = document.querySelectorAll('.view-more a');
+viewMoreLinks.forEach(function(link) {
+    if (!resultsFound) {
+        link.style.display = 'none';
+    } else {
+        link.style.display = 'block';
+    }
+});
+
+// Hide the pagination if there is text in the search input
+var pagination = document.querySelector('.pagination');
+var copyrightContainer = document.querySelector('.copyright.container');
+if (input.value.length > 0) {
+    pagination.style.display = 'none';
+    copyrightContainer.style.display = 'none';
+} else {
+    pagination.style.display = 'block';
+    copyrightContainer.style.display = 'block';
+}
