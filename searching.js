7ad75225d1d73ff43acf36d57f9a483d0996f920
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-14 20:54:26 -0500

    Update searching.js

--- searching.js
+++ searching.js
@@ -16,24 +16,50 @@ function searchGames() {
     // Loop through all game cards
     gameCards.forEach(function(card) {
         var title = card.querySelector('h2').textContent.toLowerCase(); // Get the game title
+        var starIcon = card.querySelector('i.fa-solid.fa-star'); // Get the star icon
+        var viewMore = card.querySelector('h5.view-more'); // Get the view more element
 
         // Check if the game title contains the search term
         if (title.includes(searchTerm)) {
             card.style.display = 'block'; // Show the card if it matches the search term
+            starIcon.style.display = 'block'; // Show the star icon
+            viewMore.style.display = 'block'; // Show the view more element
             resultsFound = true; // Set the flag to true since a result was found
         } else {
             card.style.display = 'none'; // Hide the card if it doesn't match
+            starIcon.style.display = 'none'; // Hide the star icon
+            viewMore.style.display = 'none'; // Hide the view more element
         }
     });
 
     // Hide or show the "No results found" message based on the flag
     if (resultsFound) {
         noResultsMessage.style.display = 'none'; // Hide the message if results were found
-        copyrightDiv.style.display = 'block'; // Show the copyright div if results were found
+        copyrightDiv.style.display = 'none'; // Hide the copyright div if results were found
     } else {
         noResultsMessage.style.display = 'block'; // Show the message if no results were found
         copyrightDiv.style.display = 'none'; // Hide the copyright div if no results were found
     }
+
+    // Hide or show the trending sections based on the flag
+    var trendingSections = document.querySelectorAll('.trending.container');
+    trendingSections.forEach(function(section) {
+        if (!resultsFound) {
+            section.style.display = 'none'; // Hide the section if no results were found
+        } else {
+            section.style.display = 'block'; // Show the section if results were found
+        }
+    });
+
+    // Hide the <a> elements with class "view-more" if no results were found
+    var viewMoreLinks = document.querySelectorAll('.view-more a');
+    viewMoreLinks.forEach(function(link) {
+        if (!resultsFound) {
+            link.style.display = 'none';
+        } else {
+            link.style.display = 'block';
+        }
+    });
 
     // Hide the pagination if there is text in the search input
     var pagination = document.querySelector('.pagination');
@@ -51,33 +77,6 @@ searchInput.addEventListener('input', searchGames);
 // Initially hide the copyright div
 var copyrightDiv = document.querySelector('.copyright.container');
 copyrightDiv.style.display = 'none';
-
-// Function to reset search and show all elements
-function resetSearch() {
-    var gameCards = document.querySelectorAll('.swiper-slide');
-    gameCards.forEach(function(card) {
-        card.style.display = 'block'; // Show all game cards
-    });
-    var noResultsMessage = document.querySelector('.no-results-message');
-    noResultsMessage.style.display = 'none'; // Hide the "No results found" message
-    var pagination = document.querySelector('.pagination');
-    pagination.style.display = 'block'; // Show the pagination
-    var viewMoreLinks = document.querySelectorAll('.view-more a');
-    viewMoreLinks.forEach(function(link) {
-        link.style.display = 'block'; // Show the "View more" links
-    });
-    var specialElements = document.querySelectorAll('.fa-solid.fa-star, h2:contains("Sports"), h2:contains("Shooter"), h2:contains("Grind"), h2:contains("Emulations"), h2:contains("Misc"), h2:contains("IO Games"), h2:contains("Pizzeria"), h2:contains("Flash")');
-    specialElements.forEach(function(element) {
-        element.style.display = 'block'; // Show the special elements
-    });
-}
-
-// Add an event listener to reset the search
-var resetButton = document.querySelector('.reset-button');
-resetButton.addEventListener('click', function() {
-    searchInput.value = ''; // Clear the search input
-    resetSearch();
-});
 
 
 
