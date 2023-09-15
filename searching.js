074199ef4caa9f20a5c630a3859b7712926ad885
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-14 20:45:43 -0500

    Update searching.js

--- searching.js
+++ searching.js
@@ -29,6 +29,7 @@ function searchGames() {
     // Hide or show the "No results found" message based on the flag
     if (resultsFound) {
         noResultsMessage.style.display = 'none'; // Hide the message if results were found
+        copyrightDiv.style.display = 'block'; // Show the copyright div if results were found
     } else {
         noResultsMessage.style.display = 'block'; // Show the message if no results were found
         copyrightDiv.style.display = 'none'; // Hide the copyright div if no results were found
@@ -54,12 +55,6 @@ function searchGames() {
         }
     });
 
-    // Hide specific elements that should not be displayed when searching
-    var specialElements = document.querySelectorAll('.fa-solid.fa-star, h2:contains("Sports"), h2:contains("Shooter"), h2:contains("Emulations"), h2:contains("Misc")');
-    specialElements.forEach(function(element) {
-        element.style.display = 'none';
-    });
-
     // Hide the pagination if there is text in the search input
     var pagination = document.querySelector('.pagination');
     if (input.value.length > 0) {
@@ -68,6 +63,15 @@ function searchGames() {
         pagination.style.display = 'block';
     }
 }
+
+// Add an event listener to the search input
+var searchInput = document.querySelector('.search-txt');
+searchInput.addEventListener('input', searchGames);
+
+// Initially hide the copyright div
+var copyrightDiv = document.querySelector('.copyright.container');
+copyrightDiv.style.display = 'none';
+
 
 
 
