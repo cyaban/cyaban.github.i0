1c9fd0f52c854409ee9a895e589ad2af67a284e9
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-19 09:16:19 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,7 +1,7 @@
 
    self.__uv$config = {
     prefix: "/uv/service/",
-    bare: "https://21.yourmom.eu.org/content/",
+    bare: "https://2.yourmom.eu.org/content/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: "/uv/uv.handler.js",
