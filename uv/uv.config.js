7954f6e2176212f81011cf3470168146eb50321b
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-20 22:27:26 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://stingrayproxy.onrender.com/bare/',
+    bare: 'https://stopitrend3r.onrender.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
