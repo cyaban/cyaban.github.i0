960558ea75a42006b3572f53ad8f6016118445a6
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-25 06:29:27 -0500

    Update blank.js

--- blank.js
+++ blank.js
@@ -1,22 +1,13 @@
 const installButton = document.querySelector('.blank-button');
 
 installButton.addEventListener('click', () => {
-  // Get the URL of the plugin to install.
-  const pluginUrl = installButton.dataset.url;
+  // Open https://fusionw.pages.dev/ in about blank.
+  window.open('https://fusionw.pages.dev/', '_blank', 'noreferrer noopener');
 
-  // Create a new script element.
-  const scriptElement = document.createElement('script');
-
-  // Set the source of the script element to the plugin URL.
-  scriptElement.src = pluginUrl;
-
-  // Append the script element to the head of the document.
-  document.head.appendChild(scriptElement);
-
-  // Redirect to ixl.com after the plugin has been loaded.
-  scriptElement.onload = () => {
+  // Redirect to ixl.com after 1 second.
+  setTimeout(() => {
     window.location.replace('https://www.ixl.com/');
-  };
+  }, 1000);
 });
 
 
