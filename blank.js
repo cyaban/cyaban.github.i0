9c31a5596b48f661b2be2525ba3f2c911ab52bf9
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-25 09:47:50 -0500

    Update blank.js

--- blank.js
+++ blank.js
@@ -1,49 +1,16 @@
-function openTabAndRedirectToIxl() {
-  console.log('openTabAndRedirectToIxl() called');
+function openInIframe(siteUrl) {
+  // Create a new iframe element.
+  const iframe = document.createElement('iframe');
 
-  // Create a new window object.
-  var win = window.open();
+  // Set the iframe's src attribute to the site's URL.
+  iframe.src = siteUrl;
 
-  // Set the margin of the new window body to 0.
-  win.document.body.style.margin = '0';
+  // Set the iframe's sandbox attribute to restrict the iframe's access to the parent page.
+  iframe.sandbox = 'allow-scripts';
 
-  // Set the height of the new window body to 100vh.
-  win.document.body.style.height = '100vh';
+  // Append the iframe element to the document body.
+  document.body.appendChild(iframe);
 
-  // Create an iframe element.
-  var iframe = win.document.createElement('iframe');
-
-  // Set the border of the iframe element to none.
-  iframe.style.border = 'none';
-
-  // Set the width of the iframe element to 100%.
-  iframe.style.width = '100%';
-
-  // Set the height of the iframe element to 100%.
-  iframe.style.height = '100%';
-
-  // Set the margin of the iframe element to 0.
-  iframe.style.margin = '0';
-
-  // Set the source of the iframe element to the current URL.
-  iframe.src = url;
-
-  // Append the iframe element to the body of the new window.
-  win.document.body.appendChild(iframe);
-
-  // Replace the current page in the browser history with the ixl.com website.
-  window.location.replace("https://ixl.com");
-
-  // Create an interval to check if the new tab has been closed.
-  var interval = setInterval(function() {
-    if (win.closed) {
-      // Clear the interval if the new tab has been closed.
-      clearInterval(interval);
-
-      // Set the win variable to undefined.
-      win = undefined;
-    }
-  }, 500);
+  // Open the about:blank page.
+  window.open('about:blank');
 }
-
-
