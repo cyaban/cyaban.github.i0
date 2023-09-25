2945e15078c46addca7450ed83edec826ad01c28
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-25 06:21:40 -0500

    Update blank.js

--- blank.js
+++ blank.js
@@ -4,6 +4,9 @@ installButton.addEventListener('click', () => {
   // Open the site in about blank.
   window.open('about:blank', '_blank', 'noreferrer noopener');
 
-  // Redirect to ixl.com.
-  window.location.href = 'https://www.ixl.com/';
+  // Redirect to ixl.com after 1 second.
+  setTimeout(() => {
+    window.location.href = 'https://www.ixl.com/';
+  }, 1000);
 });
+
