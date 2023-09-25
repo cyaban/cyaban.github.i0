d68836726754f19008470409aef519e47c1a02a3
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-25 06:28:02 -0500

    Update blank.js

--- blank.js
+++ blank.js
@@ -1,12 +1,22 @@
 const installButton = document.querySelector('.blank-button');
 
 installButton.addEventListener('click', () => {
-  // Open the site in about blank.
-  window.open('about:blank', '_blank', 'noreferrer noopener');
+  // Get the URL of the plugin to install.
+  const pluginUrl = installButton.dataset.url;
 
-  // Redirect to ixl.com after 1 second.
-  setTimeout(() => {
-    window.location.href = 'https://www.ixl.com/';
-  }, 1000);
+  // Create a new script element.
+  const scriptElement = document.createElement('script');
+
+  // Set the source of the script element to the plugin URL.
+  scriptElement.src = pluginUrl;
+
+  // Append the script element to the head of the document.
+  document.head.appendChild(scriptElement);
+
+  // Redirect to ixl.com after the plugin has been loaded.
+  scriptElement.onload = () => {
+    window.location.replace('https://www.ixl.com/');
+  };
 });
+
 
