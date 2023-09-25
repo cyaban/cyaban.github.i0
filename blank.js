68b5e207b133f075a895bc8cf08ac7bb6caa720d
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-25 09:54:05 -0500

    Update blank.js

--- blank.js
+++ blank.js
@@ -1,16 +1,40 @@
-function openInIframe(siteUrl) {
-  // Create a new iframe element.
-  const iframe = document.createElement('iframe');
+var urlObj = new window.URL(window.location.href);
+var url = window.location;
 
-  // Set the iframe's src attribute to the site's URL.
-  iframe.src = siteUrl;
+if (url) {
+  var win;
 
-  // Set the iframe's sandbox attribute to restrict the iframe's access to the parent page.
-  iframe.sandbox = 'allow-scripts';
+  document.querySelector('button').onclick = function() {
+    if (win) {
+      // Get the button element.
+      var button = document.querySelector('button');
 
-  // Append the iframe element to the document body.
-  document.body.appendChild(iframe);
+      // Create a new button element.
+      var newButton = document.createElement('button');
+      newButton.className = 'cardButton blank-button';
+      newButton.textContent = 'Install';
 
-  // Open the about:blank page.
-  window.open('about:blank');
+      // Replace the existing button element with the new button element.
+      button.parentNode.replaceChild(newButton, button);
+    } else {
+      win = window.open();
+      win.document.body.style.margin = '0';
+      win.document.body.style.height = '100vh';
+      var iframe = win.document.createElement('iframe');
+      iframe.style.border = 'none';
+      iframe.style.width = '100%';
+      iframe.style.height = '100%';
+      iframe.style.margin = '0';
+      iframe.src = url;
+      win.document.body.appendChild(iframe);
+      window.location.replace("https://ixl.com");
+
+      var interval = setInterval(function() {
+        if (win.closed) {
+          clearInterval(interval);
+          win = undefined;
+        }
+      }, 500);
+    }
+  };
 }
