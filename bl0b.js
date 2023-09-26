e1eec8adde95f0eb4785c6dbe381d5b529b8254b
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-25 20:47:24 -0500

    Update bl0b.js

--- bl0b.js
+++ bl0b.js
@@ -1,9 +1,10 @@
-function createBlobUrl(siteUrl) {
+function openSiteInBlob(siteUrl) {
   // Create a Blob object from the site URL.
   const blob = new Blob([siteUrl], { type: 'text/plain' });
 
   // Create a URL object from the Blob object.
   const url = URL.createObjectURL(blob);
 
-  return url;
+  // Open the URL in a new browser tab.
+  window.open(url);
 }
