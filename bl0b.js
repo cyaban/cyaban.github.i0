f1406b579aa1d8a80d4e26b1b04307ab966adbe9
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-25 22:15:26 -0500

    Update bl0b.js

--- bl0b.js
+++ bl0b.js
@@ -1,23 +1,19 @@
-function openSiteInBlob(url) {
-  let button = document.querySelector('.cardButton.install-button');
-  let viewport = document.querySelector('#theaterViewport');
-
-  // Get the URL of the site to install from the button's `data-type` attribute.
-  let siteUrl = button.dataset.type;
-
-  // Create a blob of the site's HTML code.
-  let blob = new Blob(["<html><body><iframe src='{}' style='width: 100%; height: 100%; border: none; overflow: hidden;'></iframe></body></html>".format(siteUrl)], "text/html");
+function installWebsite(url) {
+  // Create a blob of the website's HTML code.
+  let blob = new Blob(["<html><body><iframe src='{}' style='width: 100%; height: 100%; border: none; overflow: hidden;'></iframe></body></html>".format(url)], "text/html");
 
   // Create a URL for the blob.
   let blobUrl = URL.createObjectURL(blob);
 
-  // Set the `src` attribute of the viewport iframe to the blob URL.
-  viewport.src = blobUrl;
+  // Set the `src` attribute of an iframe element to the blob URL.
+  let iframe = document.createElement("iframe");
+  iframe.src = blobUrl;
 
-  // Request fullscreen mode for the viewport.
-  viewport.requestFullscreen();
+  // Request fullscreen mode for the iframe element.
+  iframe.requestFullscreen();
+
+  // Append the iframe element to the document body.
+  document.body.appendChild(iframe);
 }
 
-// Add the following event listener to the button:
-document.querySelector('.cardButton.install-button').addEventListener('click', openSiteInBlob);
 
