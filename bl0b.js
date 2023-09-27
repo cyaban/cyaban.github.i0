49b472f0fe48a7508cfdee5d4f97fe13d90158a4
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-09-26 20:23:21 -0500

    Update bl0b.js

--- bl0b.js
+++ bl0b.js
@@ -1,20 +1,7 @@
-function handleFullscreenClick(){
-  let e = document.querySelector('.cardButton.install-button');
-  e.requestFullscreen?e.requestFullscreen():e.mozRequestFullScreen?e.mozRequestFullScreen():e.webkitRequestFullscreen?e.webkitRequestFullscreen():e.msRequestFullscreen&&e.msRequestFullscreen();
-}
+import { createBlobURL } from 'blob-util';
 
-function Popout(){
+function BlobPopout(){
   let e = document.querySelector('.cardButton.install-button').getAttribute('src');
-  window.open(e);
-}
-
-function ABPopout(){
-  let e = document.querySelector('.cardButton.install-button').getAttribute('src');
-  var t=window.open('about:blank'),o=document.createElement('iframe');
-  o.src=window.location.origin+e,o.style.width='100%',o.style.height='100%',o.style.border='none',o.style.overflow='hidden',t.document.body.appendChild(o);
-}
-
-function openSiteInBlob(){
-  let e = document.querySelector('.cardButton.install-button').getAttribute('src');
-  openSiteInBlob(window.location.origin+e);
+  const blobUrl = createBlobURL(`<html><body></body></html>`);
+  window.open(blobUrl, '_blank');
 }
