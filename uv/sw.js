4f88ec31239021e6bcfaa9a2fbe1240b4f640208
John <72810050+Notplayingallday383@users.noreply.github.com>
2023-09-30 11:48:05 -0400

    UV Upgrade

--- uv/sw.js
+++ uv/sw.js
@@ -11,4 +11,49 @@ importScripts(__uv$config.sw || 'uv.sw.js');
 
 const sw = new UVServiceWorker();
 
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
+let bareServerUrl;
+
+async function updateBareServerUrl() {
+  const cache = await caches.open('bareServerCache');
+  const response = await cache.match('bareServerKey');
+  if (response) {
+    bareServerUrl = await response.text();
+  }
+}
+
+updateBareServerUrl();
+
+self.addEventListener('fetch', async (event) => {
+  if (bareServerUrl) {
+    const req = new Request(bareServerUrl, {
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'same-origin',
+    });
+
+    try {
+      const res = await fetch(req);
+    
+      if (self.__uv$config) {
+        self.__uv$config.bare = res.url;
+      }
+    } catch (error) {
+      console.error('Error fetching bare server URL:', error);
+    }    
+  }
+
+  event.respondWith(sw.fetch(event));
+});
+
+self.addEventListener('message', async (event) => {
+  if (event.data && event.data.action === 'updateBareServerUrl') {
+    updateBareServerUrl()
+      .then(() => {
+        console.log('Bare server URL updated');
+      })
+      .catch(error => {
+        console.error('Error updating bare server URL:', error);
+      });
+  }
+});
+
