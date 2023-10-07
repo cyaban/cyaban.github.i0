a41f2da25ca2108bab44143bca255237bd949822
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-10-06 21:52:12 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,10 @@
+document.getElementById("cards").onmousemove = e => {
+  for(const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
 
-function redirectToWebsite() {
-  const keyInput = document.getElementById('key-input');
-  const websiteInput = document.getElementById('website-input');
-  const storedKey = localStorage.getItem('panicKey');
-  const storedWebsite = localStorage.getItem('panicWebsite');
-  const defaultKey = storedKey || ']';
-  const defaultWebsite = storedWebsite || 'https://classroom.google.com';
-  keyInput.value = defaultKey;
-  websiteInput.value = defaultWebsite;
-
-  keyInput.addEventListener('input', function() {
-    localStorage.setItem('panicKey', keyInput.value);
-  });
-
-  websiteInput.addEventListener('input', function() {
-    localStorage.setItem('panicWebsite', websiteInput.value);
-  });
-
-  document.addEventListener('keyup', function(event) {
-    if (event.key.toLowerCase() === keyInput.value.toLowerCase()) {
-      let url = websiteInput.value.trim();
-      if (!url.startsWith('http://') && !url.startsWith('https://')) {
-        url = 'http://' + url;
-      }
-      window.location.href = url;
-    }
-  });
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
 }
-
-redirectToWebsite();
