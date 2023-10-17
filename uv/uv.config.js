b28c9ed7c66dc27a9552a95f63569f9b8fc8e4b6
Alphabet <118471310+DEVAbdelToom@users.noreply.github.com>
2023-10-17 12:10:08 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,11 +1,11 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://astroid.wtf/bare/',
+    bare: 'https://astroid.wtf/bare/',    # the bare server is fine the probelm is searx.degenerate.
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
     client: '/uv/uv.client.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+    sw: '/uv/uv.sw.js',    
 };
