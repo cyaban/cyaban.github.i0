c41ca772ae79298cda71adf1afbc91fe8ada439f
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-11-13 05:34:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,74 +1,74 @@
-import { createBareServer } from "@tomphttp/bare-server-node";
-import express from "express";
-import { createServer } from "node:http";
-import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
-import { join } from "node:path";
-import { hostname } from "node:os";
-import { fileURLToPath } from 'url';
-import { dirname } from 'node:path';
+import express from 'express';
+import http from 'node:http';
+import createBareServer from "@tomphttp/bare-server-node";
+import path from 'node:path';
+import cors from 'cors';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = process.cwd();
+const server = http.createServer();
+const app = express(server);
+const bareServer = createBareServer('/outerspace/');
+const PORT = 8080;
 
-const bare = createBareServer("/bare/");
-const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'static')));
 
-app.use(express.static("fusion-static"));
+const routes = [
+  { path: '/', file: 'index.html' },
+  { path: '/news', file: 'apps.html' },
+  { path: '/events', file: 'games.html' },
+  { path: '/diagnostic', file: 'settings.html' },
+  { path: '/local-news', file: 'tabs.html' },
+  { path: '/image-galleries', file: 'go.html' },
+];
 
-app.use("/uv/", express.static(uvPath));
-
-app.use((req, res, next) => {
-  res.status(404);
-  res.sendFile(join(__dirname, "/uv", "404.html"));
+app.get('/edu/*', cors({ origin: false }), async (req, res, next) => {
+  try {
+    const reqTarget = `https://raw.githubusercontent.com/InterstellarNetwork/Interstellar-Assets/main/${req.params[0]}`;
+    const asset = await fetch(reqTarget);
+    
+    if (asset.ok) {
+      const data = await asset.arrayBuffer();
+      res.end(Buffer.from(data));
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.error('Error fetching:', error);
+    next(error);
+  }
 });
 
-const server = createServer();
+routes.forEach((route) => {
+  app.get(route.path, (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', route.file));
+  });
+});
 
-server.on("request", (req, res) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeRequest(req, res);
+server.on('request', (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
   } else {
     app(req, res);
   }
 });
 
-server.on("upgrade", (req, socket, head) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeUpgrade(req, socket, head);
+server.on('upgrade', (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
   } else {
     socket.end();
   }
 });
 
-let port = parseInt(process.env.PORT || "");
-
-if (isNaN(port)) port = 8080;
-
-server.on("listening", () => {
-  const address = server.address();
-
-  console.log("Fusion is running on:");
-  console.log(`\thttp://localhost:${address.port}`);
-  console.log(`\thttp://${hostname()}:${address.port}`);
-  if (address.family === "IPv4") {
-    console.log(`\thttp://${address.address}:${address.port}`);
-  } else {
-    console.log(`\thttp://[${address.address}]:${address.port}`);
-  }  
+server.on('listening', () => {
+  console.log(`Running at http://localhost:${PORT}`);
 });
 
-process.on("SIGINT", shutdown);
-process.on("SIGTERM", shutdown);
-
-function shutdown() {
-  console.log("SIGTERM signal received: closing HTTP server");
-  server.close();
-  bare.close();
-  process.exit(0);
-}
-
 server.listen({
-  port,
+  port: PORT,
 });
 
 
